5833bb9079d11bb1994b7b316466e60b63cb76e8
TJKumaran <muthalagu1989@gmail.com>
2021-06-26 14:31:10 +0530

    Initial Commit

---
+++ WpfApp1/WpfApp1/Installer1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+
+            this.BeforeInstall += InstallerClass_BeforeInstall;
+        }
+
+        private void InstallerClass_BeforeInstall(object sender, InstallEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            MessageBox.Show("Install");
+            base.Install(stateSaver); 
+        }
+    }
+}
